95012cd4b24f6bebaefdba4de5c483ce2713c119
sam.chuang <a3587664@gmail.com>
2018-07-08 20:11:28 +0800

    add Controller

---
+++ FireWeb/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FireWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
